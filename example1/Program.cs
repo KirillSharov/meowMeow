b959635ef5a48197a88d08692b340c1a4419686a
Kirill Sharov <kirill.scharov@gmail.com>
2023-07-05 09:53:54 +0300

    initial commit

---
+++ example1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+Console.WriteLine("New programm! Hello! Let's start!");
+Console.WriteLine("Введите три числа");
+int first = int.Parse(Console.ReadLine());
+int second = int.Parse(Console.ReadLine());
+int third = int.Parse(Console.ReadLine());
+
+int sum = first + second + third;
+int mult = first * second * third;
+
+Console.WriteLine($"Сумма этих чисел равна {sum}");
+Console.Write($"Произведение этих чисел равно {mult}");
